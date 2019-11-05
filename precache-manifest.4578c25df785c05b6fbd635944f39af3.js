046121e5d99d176ba2cd13ba9d1a2a3a3e222688
vivian-he <vyang44@gmail.com>
2019-11-05 01:07:33 -0500

    Updates

---
+++ precache-manifest.4578c25df785c05b6fbd635944f39af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11417bc14ce15abb4dcbb84abcbaf13",
+    "url": "/ssui-hw6/index.html"
+  },
+  {
+    "revision": "f85ec4de8b41ba7ab024",
+    "url": "/ssui-hw6/static/css/main.e580cf6c.chunk.css"
+  },
+  {
+    "revision": "e6b17ab4c058d6c24111",
+    "url": "/ssui-hw6/static/js/2.27ddf2fd.chunk.js"
+  },
+  {
+    "revision": "f85ec4de8b41ba7ab024",
+    "url": "/ssui-hw6/static/js/main.4ff19c21.chunk.js"
+  },
+  {
+    "revision": "7f89ad9604dee8c1af32",
+    "url": "/ssui-hw6/static/js/runtime-main.f93c2361.js"
+  }
+]);
